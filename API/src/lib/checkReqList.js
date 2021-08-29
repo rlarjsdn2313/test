f5dc16256c13cb10da0a6d26d44d5b303930dc5c
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-08-29 13:41:43 +0900

    upgrade checkReqList

--- API/src/lib/checkReqList.js
+++ API/src/lib/checkReqList.js
@@ -1,9 +1,52 @@
-var checkReqList = (reqList, neededList) => {
+const e = require("express");
+
+var checkReqList = (req, neededList) => {
+    let reqList = Object.keys(req);
+
     for (var i=0; i<neededList.length; i++) {
         var check = false;
 
         for (var a=0; a<reqList.length; a++) {
             if (reqList[a] == neededList[i]) {
+                // grade
+                if (neededList[i] == 'grade') {
+                    if (typeof(req[reqList[a]]) != 'number') {
+                        return false;
+                    }
+                    else if (!(1<=req[reqList[a]]<=3)) {
+                        return false;
+                    }
+                }
+
+                // semester
+                else if (neededList[i] == 'semester') {
+                    if (req[reqList[a]] != '1st' && req[reqList[a]] != '2nd') {
+                        return false;
+                    }
+                }
+
+                // exam
+                else if (neededList[i] == 'exam') {
+                    if (req[reqList[a]] != 'midterm' && req[reqList[a]] != 'final') {
+                        return false;
+                    }
+                }
+
+                // subject
+                else if (neededList[i] == 'subject') {
+                    var subjects = ['수학', '과학', '사회', '역사', '정보', '사회', '체육', '음악', '한문', '미술', '영어', '국어', '도덕']
+                    if (!(req[reqList[a]] in subjects)) {
+                        return false;
+                    }
+                }
+
+                // year
+                else if (neededList[i] == 'year') {
+                    if (typeof(year) != 'number') {
+                        return false;
+                    }
+                }
+                
                 check = true;
                 break;
             }
--- API/src/router/main.js
+++ API/src/router/main.js
@@ -10,7 +10,9 @@ router.post('/', (req, res) => {
     /*
     1. check ReqList
     */
-    if (!checkReqList(Object.keys(req.body), ['grade', 'semester', 'exam', 'subject', 'year'])) {
+    if (!checkReqList(
+        Object.keys(req.body),
+        ['grade', 'semester', 'exam', 'subject', 'year'])) {
         res.send({
             error: true,
             message: 'Request Error'
