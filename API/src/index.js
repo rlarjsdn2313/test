df1ef273ed8b475c0389ac7c5cabf78c85c2aaef
rlarjsdn2313 <rlarjsdn2313@github.com>
2021-08-29 12:58:45 +0900

    index.js basic setting

---
+++ API/src/index.js
@@ -0,0 +1,13 @@
+// import express framework
+const express = require('express');
+const app = express();
+
+// listening port
+const port = 3000;
+
+
+// listening
+app.listen(port, () => {
+    console.log(`Jangseung Test API 1.0`);
+    console.log(`Server Is Listening (port: ${port})`);
+});
